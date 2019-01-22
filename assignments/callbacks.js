3ffcfaf7ec63876a67270ee2f4660485c307f45a
Frank Martinez <frankie.martinez@gmail.com>
2019-01-21 22:55:14 -0800

    finished array-methods.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -15,72 +15,61 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const list = ['Pencil', 'paper', 'bicycle', 'notebook', 'ruler'];
+// const list = ['Pencil', 'paper', 'bicycle', 'notebook', 'ruler'];
 
 
+// Basic output callback function. basicOutput pushes whatever result from the function to the console.
+const basicOutput = (len) => { console.log(len); };
 // getLength passes the length of the array into the callback.
 function getLength(arr, cb) {
-  return cb(arr.length);
+  return cb(`The length of the array is "${arr.length}"`);
 }
-getLength(items, function(len) {
-  console.log(len);
-})
-
-
+getLength(items, basicOutput);
 // last passes the last item of the array into the callback.
 function last(arr, cb) {
-  return cb(arr[arr.length - 1]);  
+  return cb(`The last item in the array is "${arr[arr.length - 1]}"`);  
 }
-last(items, function(lastCB) {
-  console.log(lastCB);
-})
+last(items, basicOutput);
 
 
 // sumNums adds two numbers (x, y) and passes the result to the callback.
 function sumNums(x, y, cb) {
-  return cb(x+y);
+  return cb(`SUM: ${x + y}`);
 }
-sumNums(1, 2, function(sumNumsCB) {
-  console.log(sumNumsCB);
-})
-
-
+sumNums(3, 2, basicOutput);
 // multiplyNums multiplies two numbers and passes the result to the callback.
 function multiplyNums(x, y, cb) {
-  return cb(x * y);
+  return cb(`PRODUCT: ${x * y}`);
 }
-multiplyNums(3, 9, function(multNumsCB) {
-  console.log(multNumsCB);
-})
+multiplyNums(3, 9, basicOutput);
+
+
 
 // contains checks if an item is present inside of the given array/list.
 // Pass true to the callback if it is, otherwise pass false.
+// I think there are quite a few ways I could have done this more efficiently.
 function contains(item, list, cb) {
-  totalArr = [];
-  console.log(totalArr);
-  for (let i=0; i < list.length; i++) {
-    totalArr.push(list[i]);
-  }
-  totalArr.push(item);
-  console.log(totalArr);
-  totalArr.sort();
-  console.log(totalArr);
+  let totalArr = [item.toUpperCase()];
+  let match = false;
+  
+  list.forEach((itemInList) => {
+    totalArr.push(itemInList.toUpperCase());
+    totalArr.sort();
+  });
+
   for (let i=0; i < totalArr.length; i++) {
     if (totalArr[i] === totalArr[i + 1]) {
-      console.log(totalArr);
-      return true;
-    } else {
-      return false;
+      match = true;
     }
   }
-   
+
+  return cb(match);
 }
-contains('Notebook', items, function(containsCB) {
-  console.log(containsCB);
-})
+contains('notebook', items, basicOutput);
+
+
 
 /* STRETCH PROBLEM */
-
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
