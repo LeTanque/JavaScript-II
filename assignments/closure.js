015497fa73893cc3c95c4dfd6d0dc54a2d1a89f0
Frank Martinez <frankie.martinez@gmail.com>
2019-01-22 14:14:10 -0800

    stretch goal on closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,20 +1,108 @@
 // ==== Challenge 1: Write your own closure ====
-// Write a simple closure of your own creation.  Keep it simple!
+// // Write a simple closure of your own creation.  Keep it simple!
+// function kingdom(title) {
+//   console.log(`Ye ole kingdom of ~${title}~`);
+//   function town() {
+//     console.log(`Welcome to the township of ${title}shire`);
+//   }
+//   town();
+// }
+// kingdom('Dragonscale');
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// // Return a function that when invoked increments and returns a counter variable.
+// const counter = () => {
+//   let count = 8100;
+//   // console.count(`Power level`);
+//   // return count += 1;
+//   return function() {
+//     count += 10;
+//     // console.count(``);
+//     console.log(`Power level:`);
+//     return count;
+    
+//   }
+// };
+// // console.log(counter());
+// // console.log(counter());
+// // console.log(counter());
+// // console.log(counter());
+// const newCounter = counter();
+// console.log(newCounter()); 
+// console.log(newCounter()); 
+// console.log(newCounter()); 
+// console.log(newCounter()); 
+// console.log(`~~~ Almost there! ~~~`);
+
+// // Function to increment counter ES5
+// // not really a counter though. console.log is tracking invocation, 
+// // but the value of each previous iteration is not actually being stored (?)
+// let counter = 0;
+// function add() {
+//   counter += 1;
+//   console.log(counter);
+// }
+// add();
+// add();
+// add();
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
+// const counterFactory = (start, direction) => {
+//   let count = start;
+//   if (direction = 'up') {
+//     return function() {
+//       count += 1;
+//       return count;
+//     }
+//   } else if (direction = 'down') {
+//     return function() {
+//       count -= 1;
+//       return count;
+//     }
+//   } else {
+//     return 'choose either up or down';
+//   }
+  
+// };
+
+// const countFact = counterFactory(0,up);
+// console.log(countFact());
+// console.log(countFact());
+// console.log(countFact());
+// console.log(countFact());
+
+// // Ok, this isn't working.  ... reads MDN ... meditates ...
+// // Challenge 3 Round 2
+const counter = (function() {
+  let countDracula = 0;
+  function changeBy(val) {
+    countDracula += val;
+  }
+  return {
+    increment: () => {
+      changeBy(1);
+      return `${countDracula} banana, ha. ha. ha.`;
+    },
+    decrement: () => {
+      changeBy(-1);
+      return `${countDracula} banana, ha. ha. ha.`;
+    },
+    value: function() {
+      return `Count Dracula's current value is ${countDracula}`;
+    }
+  };   
+}) ();
+
+console.log(counter.increment());
+console.log(counter.decrement());
+console.log(counter.value()); 
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.increment());
--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -1,23 +1,30 @@
+// This is my answer to the same problem in JSI
+
+
+exampleArray = [1,2,3,4];
+
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
+let myFunction = () => {};
 
-// let myFunction = function () {};
+let anotherFunction = (param) => {
+  return param;
+};
+anotherFunction('hello');
 
-// let anotherFunction = function (param) {
-//   return param;
-// };
 
-// let add = function (param1, param2) {
-//   return param1 + param2;
-// };
-// add(1,2);
+let add = (param1, param2) => {
+    return param1 + param2;
+};
+add(1,2);
 
-// let subtract = function (param1, param2) {
-//   return param1 - param2;
-// };
-// subtract(1,2);
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+let subtract = (param1, param2) => {
+    return param1 - param2;
+};
+subtract(1,2);
+
+
+const triple = exampleArray.map((num) => {
+  return num * 3;
+});
+console.log(triple);
